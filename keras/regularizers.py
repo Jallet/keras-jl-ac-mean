b2a70fd2a837073d4a2eb507ac411d7ee1046ada
Liang Jiang <jal@mail.ustc.edu.cn>
2016-07-25 16:41:55 +0800

    add covariance regularizer on activations

--- keras/regularizers.py
+++ keras/regularizers.py
@@ -23,7 +23,7 @@ class WeightRegularizer(Regularizer):
     def __init__(self, l1=0., l2=0.):
         self.l1 = K.cast_to_floatx(l1)
         self.l2 = K.cast_to_floatx(l2)
-        self.ld = 2e-4
+        self.l2 = K.cast_to_floatx(0.)
         self.uses_learning_phase = True
 
     def set_param(self, p):
@@ -39,52 +39,8 @@ class WeightRegularizer(Regularizer):
                             'ActivityRegularizer '
                             '(i.e. activity_regularizer="l2" instead '
                             'of activity_regularizer="activity_l2".')
-        # print "type of p: ", type(self.p)
-        # a = self.p.get_value()
-        # print "type of a: ", type(a)
-        # print "shape of a: ", a.shape
-        
         regularized_loss = loss + K.sum(K.abs(self.p)) * self.l1
         regularized_loss += K.sum(K.square(self.p)) * self.l2
-        # regularized_loss += K.sum(K.pow(self.p, 4)) * 1000
-        print "type of self.p: ", type(self.p)
-        if 4 == self.p.ndim:
-            p_array = self.p.get_value() 
-            print "ndim of self.p: ", self.p.ndim
-            print "shape of self.p: ", p_array.shape
-            shape = self.p.shape
-            size = self.p.size
-            row = shape[0]
-            col = size / row
-            print "type of row: ", row.type, ", type of col: ", col.type
-            reshaped_p = K.reshape(self.p, (row, col))
-            p_mean = K.mean(reshaped_p, axis = 1, keepdims = True)
-            centered_p = reshaped_p - p_mean
-            centered_p_t = K.transpose(centered_p)
-            covariance = K.dot(centered_p, centered_p_t)
-            print "type of covariance: ", type(covariance)
-            print "type of covariance: ", covariance.type
-            mask = T.eye(row)
-            regularized_loss += K.sum(K.abs(covariance - mask * covariance)) * self.ld
-        # mask = K.ones_like(covariance) - K.eye(row)
-        # diversity_loss = K.sum(covariance * mask)
-        # regularized_loss += diversity_loss
-
-
-        # print "type of regularized_loss: ", type(regularized_loss)
-        # a = K.sum(K.square(self.p)) * self.l2
-        # print "type of a: ", type(a)
-        # p_array = self.p.get_value() 
-        # if 4 == p_array.ndim:
-        #     p_reshape = p_array.reshape((p_array.shape[0], 
-        #         p_array.size / p_array.shape[0]))
-        #     covariance = np.asmatrix(p_reshape) * np.asmatrix(p_reshape.transpose())
-        #     diversity_loss = covariance.sum() - covariance.trace()
-        #     diversity_loss = diversity_loss[0, 0]
-        #     # print "shape of diversity_loss: ", diversity_loss.shape
-        #     diversity_loss = K.variable(diversity_loss)
-        #     print "type of diversity_loss: ", type(diversity_loss)
-        #     regularized_loss += diversity_loss
         return K.in_train_phase(regularized_loss, loss)
 
     def get_config(self):
@@ -97,6 +53,7 @@ class ActivityRegularizer(Regularizer):
     def __init__(self, l1=0., l2=0.):
         self.l1 = K.cast_to_floatx(l1)
         self.l2 = K.cast_to_floatx(l2)
+        self.ld = K.cast_to_floatx(0.5)
         self.uses_learning_phase = True
 
     def set_layer(self, layer):
@@ -112,6 +69,23 @@ class ActivityRegularizer(Regularizer):
             output = self.layer.get_output_at(i)
             regularized_loss += self.l1 * K.sum(K.mean(K.abs(output), axis=0))
             regularized_loss += self.l2 * K.sum(K.mean(K.square(output), axis=0))
+            
+            print "ndim of output: ", output.ndim
+            print "len of shape of output: ", len(self.layer.output_shape)
+            row = self.layer.output_shape[0]
+            col = 1
+            for i in range(1, len(self.layer.output_shape)):
+                print "i: ", i
+                col = col * self.layer.output_shape[i]
+            print "row: ", row
+            print "col: ", col
+            mean = K.mean(output, axis = 0, keepdims = True)
+            std = K.std(output, axis = 0, keepdims = True)
+            normalized_output = (output - mean) 
+            covariance = T.dot(T.transpose(normalized_output), normalized_output) / col
+            mask = T.eye(col)
+            regularized_loss += K.sum(K.square(covariance - mask * covariance)) * self.ld / (col - 1)
+            
         return K.in_train_phase(regularized_loss, loss)
 
     def get_config(self):
@@ -130,6 +104,10 @@ def l2(l=0.01):
 
 def l1l2(l1=0.01, l2=0.01):
     return WeightRegularizer(l1=l1, l2=l2)
+
+def l1l2ld(l1 = 0.01, l2 = 0.01, ld = 0.01):
+    print "l1l2ld"
+    return WeightRegularizer(l1 = l1, l2 = l2, ld = ld)
 
 
 def activity_l1(l=0.01):
