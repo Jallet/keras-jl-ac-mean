51d12a0b60ea3ebb2a19ab8592c561ab9ccfc6a9
Liang Jiang <jal@mail.ustc.edu.cn>
2016-08-31 12:59:40 +0800

    [cifar10][cnn-32-32-64-64-512][l2=0.001][dr=0.01][acc=0.89][val_acc=0.865]With drop out after dense, with dr in dense and softmax and conv, mean

--- examples/cifar10_cnn.py
+++ examples/cifar10_cnn.py
@@ -75,12 +75,12 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 # model.add(Dropout(0.25))
 
 model.add(Flatten())
-model.add(Dense(512, # activity_regularizer = activity_l1l2(l1 = 0., l2 = 0.), 
+model.add(Dense(512, activity_regularizer = activity_l1l2(l1 = 0., l2 = 0.), 
                         W_regularizer = l2(l = 0.), 
                         b_regularizer = l2(l = 0.)))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(nb_classes, 
+model.add(Dense(nb_classes, activity_regularizer = activity_l1l2(l1 = 0., l2 = 0.), 
                         W_regularizer = l2(l = 0.), 
                         b_regularizer = l2(l = 0.)))
 model.add(Activation('softmax'))
